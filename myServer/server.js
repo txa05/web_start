9d819c793acf3f33b1d2e1e4e1de4e04fef72e56
txavier <tchiadeestevaox@gmail.com>
2026-06-06 16:26:54 +0100

    adicionei o método on para escutar os eventos data e end para início e fim de envio de caracteres

--- myServer/server.js
+++ myServer/server.js
@@ -1,20 +1,52 @@
-
-
 const http = require('http');
 
 const HOSTNAME = '127.0.0.1'
 const PORT = 3000
 
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-type', 'application/json');
 
-	const resp = {
-		mensagem: "Server is running",
-		status: "success"
-	};
+	res.setHeader('Acess-Control-Allow-Origin', '*');
+	res.setHeader('Acess-Control-Allow-Methods', 'POST, GET, OPTIONS');
+	res.setHeader('Acess-Control-Allow-Headers', 'Content-Type');
 
-	res.end(JSON.stringify(resp));
+	if (res.method === 'OPTIONS'){
+		res.statusCode = 200;
+		res.end();
+		return ;
+	}
+
+	if (res.method === 'POST') {
+		let body = '';
+
+		req.on('data', chunk => {
+			body += chunk;
+		});
+
+		req.on('end', () => {
+			try{
+				const receivedData = JSON.parse(body);
+				const userText = receivedData.texto;
+
+				res.statusCode = 201;
+				res.setHeader('Content-Type', 'apllications/json');
+
+				const answJSON = {
+					msg: `reeived: ${userText}`,
+					receivedContent: userText,
+					length: userText.length
+				};
+
+				res.end(JSON.stringify(answJSON));
+			}
+			catch (error){
+				res.statusCode = 400;
+				res.end(JSON.stringify({ error: "invalid JSON format" }));
+			};
+		});
+	}
+	else {
+	
+	}
 });
 
 server.listen(PORT, HOSTNAME, () => {
